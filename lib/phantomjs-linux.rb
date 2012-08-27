845a67e57cdbf22e0ac6c1151a0e2b7f81472e6f
Tomáš Dundáček <tdundacek@blueberryapps.com>
2012-08-27 11:31:53 +0200

    added proxy support (currently with fixed values for testing)

--- lib/phantomjs-linux.rb
+++ lib/phantomjs-linux.rb
@@ -1,8 +1,12 @@
 require "phantomjs-linux/version"
 
 module Phantomjs
-  def self.executable_path
-    Linux.full_path
+  def self.executable_path(proxy_type = "socks5", proxy_address = "81.0.240.113:9050")
+  	if proxy_type && proxy_address
+  		"#{Linux.full_path} --proxy-type=#{proxy_type} --proxy-address=#{proxy_address}"
+  	else
+  		Linux.full_path
+  	end
   end
 
   module Linux
